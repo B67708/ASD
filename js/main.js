02f5920272ccee2a8aab6f8c0a4f0e199c313fa8
John Cardiff-Winchell <johnwinchell86@fullsail.edu>
2013-06-06 23:54:44 -0400

    week 1 setup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 //John Cardiff-Winchell
 //ASDI 1306
 //Week 1 project
+
+$("#main").on("pageinit", function (){
+	
+});
+
+$("#add").on("pageinit", function (){
+	
+});
+
+$("#news").on("pageinit", function (){
+	
+});
+
+$("#about").on("pageinit", function (){
+	
+});
